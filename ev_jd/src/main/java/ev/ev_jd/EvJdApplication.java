5acdda069408af7be03f6f7d34a3e23c8daa8ec7
JAD128 <juandima4@gmail.com>
2026-03-25 07:39:19 -0500

    punto 1

---
+++ ev_jd/src/main/java/ev/ev_jd/EvJdApplication.java
@@ -0,0 +1,13 @@
+package ev.ev_jd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvJdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvJdApplication.class, args);
+	}
+
+}
---
+++ ev_jd/src/test/java/ev/ev_jd/EvJdApplicationTests.java
@@ -0,0 +1,13 @@
+package ev.ev_jd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvJdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
